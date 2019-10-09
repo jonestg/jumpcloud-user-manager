6a91b6bf15c3f136267e6e5daa6883f84fc34da5
Tim Jones <timothygenejones@gmail.com>
2019-10-08 22:07:35 -0600

    Add createUser to vuex

--- src/store.js
+++ src/store.js
@@ -4,13 +4,18 @@ import SystemUserService from './services/systemUserService'
 const GET_USERS_START = 'GET_USERS_START'
 const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS'
 const GET_USERS_ERROR = 'GET_USERS_ERROR'
+const CREATE_USER_START = 'CREATE_USER_START'
+const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS'
+const CREATE_USER_ERROR = 'CREATE_USER_ERROR'
 
 export function createStore() {
   return new Vuex.Store({
     state: {
       users: [{id: 1, name: 'Foo'}],
       usersLoading: false,
-      usersError: false
+      usersError: false,
+      createUserLoading: false,
+      createUserError: false,
     },
     mutations: {
       [GET_USERS_START] (state) {
@@ -23,7 +28,17 @@ export function createStore() {
       [GET_USERS_ERROR] (state, { users }) {
         state.usersLoading = false
         state.usersError = true
-        state.users = users
+        state.users = [...users]
+      },
+      [CREATE_USER_START] (state) {
+        state.createUserLoading = true
+      },
+      [CREATE_USER_SUCCESS] (state) {
+        state.createUserLoading = false
+      },
+      [CREATE_USER_ERROR] (state) {
+        state.createUserLoading = false
+        state.createUserError = true
       }
     },
     actions: {
@@ -32,6 +47,12 @@ export function createStore() {
         SystemUserService.getUsers()
           .then( users => commit(GET_USERS_SUCCESS, { users }))
           .catch( () => commit(GET_USERS_ERROR))
+      },
+      createUser ({ commit }, { user }) {
+        commit(CREATE_USER_START)
+        SystemUserService.createUser(user)
+          .then( () => commit(CREATE_USER_SUCCESS) )
+          .catch( () => commit(CREATE_USER_ERROR))
       }
     },
   })
