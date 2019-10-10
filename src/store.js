fcd66d1dc178e4cc624f0625d1ab2da9160affe1
Tim Jones <timothygenejones@gmail.com>
2019-10-09 19:51:27 -0600

    Add GetUser to vuex

--- src/store.js
+++ src/store.js
@@ -4,6 +4,9 @@ import SystemUserService from './services/systemUserService'
 const GET_USERS_START = 'GET_USERS_START'
 const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS'
 const GET_USERS_ERROR = 'GET_USERS_ERROR'
+const GET_USER_START = 'GET_USER_START'
+const GET_USER_SUCCESS = 'GET_USER_SUCCESS'
+const GET_USER_ERROR = 'GET_USER_ERROR'
 const CREATE_USER_START = 'CREATE_USER_START'
 const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS'
 const CREATE_USER_ERROR = 'CREATE_USER_ERROR'
@@ -12,10 +15,16 @@ export function createStore() {
   return new Vuex.Store({
     state: {
       users: [{id: 1, name: 'Foo'}],
+      user: {},
       usersLoading: false,
       usersError: false,
+      userLoading: false,
+      userError: false,
       createUserLoading: false,
       createUserError: false,
+    },
+    getters: {
+      getUserById: (state) => (id) => state.user[id]
     },
     mutations: {
       [GET_USERS_START] (state) {
@@ -26,6 +35,22 @@ export function createStore() {
         state.users = users
       },
       [GET_USERS_ERROR] (state, { users }) {
+        state.usersLoading = false
+        state.usersError = true
+        state.users = [...users]
+      },
+      [GET_USER_START] (state) {
+        state.userLoading = true
+      },
+      [GET_USER_SUCCESS] (state, { user }) {
+        state.userLoading = false
+        // Store the user on an index to help prevent errors around stale data and race conditions
+        state.user = {
+          ...state.user,
+          [user.id]: { ...user }
+        }
+      },
+      [GET_USER_ERROR] (state, { users }) {
         state.usersLoading = false
         state.usersError = true
         state.users = [...users]
@@ -47,6 +72,12 @@ export function createStore() {
         SystemUserService.getUsers()
           .then( users => commit(GET_USERS_SUCCESS, { users }))
           .catch( () => commit(GET_USERS_ERROR))
+      },
+      getUser ({ commit }, { id } ) {
+        commit(GET_USERS_START)
+        SystemUserService.getUser(id)
+          .then( (user) => commit(GET_USER_SUCCESS, { user }))
+          .catch()
       },
       createUser ({ commit }, { user }) {
         commit(CREATE_USER_START)
