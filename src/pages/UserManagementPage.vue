835e5599ef9ee9d95be71f3a2eef89c4d19e24a1
Tim Jones <timothygenejones@gmail.com>
2019-10-09 23:06:38 -0600

    Clean up UserManagementPage Styles

--- src/pages/UserManagementPage.vue
+++ src/pages/UserManagementPage.vue
@@ -1,10 +1,6 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-    >
-      <UserListCard />
-    </v-layout>
+  <v-container class="content d-flex flex-column justify-start">
+    <UserListCard />
   </v-container>
 </template>
 
@@ -24,21 +20,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 .content {
-  margin: 20px
+  margin: 20px;
+  height: 100%;
 }
 </style>
